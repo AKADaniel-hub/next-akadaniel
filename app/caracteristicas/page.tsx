125f2edd80418955e1f83ce5c377b39513c21e65
AKADaniel-hub <156716013+AKADaniel-hub@users.noreply.github.com>
2025-11-15 18:33:11 +0000

    tecnologias criado +-

--- app/caracteristicas/page.tsx
+++ app/caracteristicas/page.tsx
@@ -1,11 +1,27 @@
 export default function page() {
 
+
+    const caracteristicas = [
+        'JSX, sintaxe que mistura HTML e JS.',
+        'Componentes, funções que retornam JSX.',
+        'Componentes Reutilizáveis e Modulares.',
+        'Roteamento Automático e APIs.',
+        'Hooks: useState, useEffect e useSWR.',
+        'Renderização Rápida e SEO Friendly.',
+        'TypeScript Seguro e Escalável.',
+        'Comunidade Ativa e Popularidade.'
+    ]
+
     return (
-    
+
 
         <>
             <h2>/caracteristicas</h2>
             <p>esta pagina está dentro da rota "/caracteristicas"</p>
+
+            {caracteristicas.map((caracteristicas, i) => (
+                <li key={i}>{caracteristicas}</li>
+            ))}
         </>
     )
-}   
+}       
---
+++ app/tecnologias/page.tsx
@@ -0,0 +1,13 @@
+import tecnologias from '@/app/data/tecnologias.json';
+
+export default function page() {
+
+    return (
+        <>
+            <h2>Tecnologias Exploradas</h2>
+                    <image></image>
+
+        </>
+    )
+
+}
