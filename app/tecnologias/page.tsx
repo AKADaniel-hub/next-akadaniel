017cc57b640bc5bf41008045f8c68693e7086f42
AKADaniel-hub <156716013+AKADaniel-hub@users.noreply.github.com>
2025-11-18 09:47:58 +0000

    algumas alteracoes na pagina tecnologias

--- app/tecnologias/page.tsx
+++ app/tecnologias/page.tsx
@@ -1,13 +1,34 @@
+import Image from 'next/image';
 import tecnologias from '@/app/data/tecnologias.json';
+
+
 
 export default function page() {
 
-    return (
-        <>
-            <h2>Tecnologias Exploradas</h2>
-                    <image></image>
 
-        </>
+
+    return (
+        <div>
+            <h1 style={{
+                fontSize: 20
+            }}><strong>Tecnologias Aprendidas:</strong></h1>
+
+            <div className="grid grid-cols-2 gap-4 mt-4">
+                {tecnologias.map((tech, i) => (
+                    <div key={i} className="bg-white p-4 rounded-lg shadow">
+                        <Image
+                            src={`/tecnologias/${tech.image}`}
+                            alt={`Logotipo do ${tech.title}`}
+                            width={200}
+                            height={200}
+                        />
+                        <h3 className="font-bold mt-2">{tech.title}</h3>
+                        <p className="text-sm">{tech.description}</p>
+                        <p className="text-yellow-500">{'⭐'.repeat(tech.rating)}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 
 }
